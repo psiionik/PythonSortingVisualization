5f4d1d43b2cbc1e13b38373db1f8da8408f1a322
psiionik <bk2dh@virginia.edu>
2025-07-23 19:58:44 -0400

    Created relative imports to different files

--- visualizer/sortingVisualizer.py
+++ visualizer/sortingVisualizer.py
@@ -1,22 +1,20 @@
 import random
 
-from visualizer import Visualizer
+from baseVisualizer import Visualizer
 
 class SortingVisualizer(Visualizer):
-    def __init__(self, length, frame_rate, seed = 42):
+    def __init__(self, length, frame_rate, seed = None):
         self.data = [i for i in range(length)]
         self.number_of_operations = 0
 
-        random.seed(seed)
+        if seed:
+            random.seed(seed)
+
         self._shuffle_data()
 
     def _shuffle_data(self):
         random.shuffle(self.data)
     
 
-
-
-
-
 if __name__ == "__main__":
-    print(SortingVisualizer(5, 60).data)
+    print(SortingVisualizer(5, 60, 42).data)
